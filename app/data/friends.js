b3b322d45f410670b5c7c60a964b3ae28b5d6343
KSK <kurtknosala@gmail.com>
2019-09-12 13:55:30 -0600

    added seed data to database

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendArry = [
+    {
+    "name":"Terry",
+    "photo":"https://www.biography.com/.image/t_share/MTE5NDg0MDYwNjkzMjY3OTgz/terry-crews-headshot-600x600jpg.jpg",
+    "scores":[
+        4,
+        1,
+        3,
+        5,
+        5,
+        4,
+        3,
+        2,
+        5,
+        1
+      ]
+  }
+]
+
+module.exports = friendArry;
